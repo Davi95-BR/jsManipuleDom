0c414b3829ce0992d28ce385f6552dfa166f8db9
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-03-25 08:46:54 -0300

    Criando,formatando e exibindo data no form

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,44 @@
 import BotaoConclui from './componentes/concluirTarefa.js'
 import BotaoDeleta from './componentes/deletarTarefa.js'
 
-	const CriarTarefa = (evento) => {
+    const handlerNovoitem = (evento) => {
+		evento.preventDefault()  //previnir o comportamento padrão do form
+		const lista = document.querySelector('[data-list]') 
+		const input = document.querySelector('[data-form-input]')  
+		const valor = input.value // apenas o valor 
 
-		evento.preventDefault()  //  previnir o comportamento padrão do formulário
+		const calendario = document.querySelector('[ data-form-date]') //Percorrendo o Dom acessando data-form-date
+		const data = moment(calendario.value) 	// Apenas o valor | moment é formato de datas 
+		const dataFormatada = data.format('DD/MM/YYYY') // Continuação da formatação data com moment
+	
+		const dados = { // Parâmetros
+			valor,
+			dataFormatada
+		}
 
-		const lista = document.querySelector('[data-list]')  // selecionando o campo e criando uma variável
-		const input = document.querySelector('[data-form-input]')  // selecionando o campo e criando uma variável
-		const valor = input.value
+		const criaTarefa = criarTarefa(dados) // Chamando a var com os parâmetros de dados
 
-		const tarefa = document.createElement('li') // criando a li
-		tarefa.classList.add('task')    //   adicionando a class css na li
+		lista.appendChild(criaTarefa)// criando uma lista em formato de filhos da ul que é o pai
+		input.value = " "      // limpando o input para + adiçôes
 
-		const conteudo = `<p class="content">${valor}</p>` //  criando o corpo da li
+	} 
 
-		tarefa.innerHTML = conteudo  //  inserindo no html o corpo com o conteúdo da li
+	const criarTarefa = ( {valor, dataFormatada})  => {
 
-		tarefa.appendChild(BotaoConclui())  //  cria a li sempre que clicar o botao
-		tarefa.appendChild(BotaoDeleta())  //  deleta a li sempre que clicar
-		lista.appendChild(tarefa)         //  criando uma lista em formato de filhos da ul que é o pai
-		input.value = " "      //   limpando o input para mais adições
-	}
+			const tarefa = document.createElement('li') 
+			tarefa.classList.add('task') // adicionando a class css na li
+			const conteudo = `<p class="content">${dataFormatada}  ${valor}</p>` //Adicionando data e string
+			tarefa.innerHTML = conteudo // inserindo no html o corpo com o conteúdo da li
+
+			tarefa.appendChild(BotaoConclui()) //  cria a li sempre que clicar o botao
+			tarefa.appendChild(BotaoDeleta()) //  deleta a li sempre que clicar
+
+			return tarefa
+		}
 
 	const novaTarefa = document.querySelector('[data-form-buttom]')  //  linkando o  botaoConclui a novaTarefa
-	novaTarefa.addEventListener('click', CriarTarefa);				 //  evento ao clicar colocar a função criarTarefa
+	novaTarefa.addEventListener('click', handlerNovoitem); //  evento ao clicar  iniciará a função
+	
 
 
 	
